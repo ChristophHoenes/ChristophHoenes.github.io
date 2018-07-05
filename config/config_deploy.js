0450bdffd736de953a79b4934ad3fac77a90a4e3
ChristophHoenes <christoph.hoenes@gmail.com>
2018-07-05 11:13:29 +0200

    change deploy file

--- config/config_deploy.js
+++ config/config_deploy.js
@@ -2,11 +2,10 @@ var config_deploy = {
 
     // obligatory fields
 
-    // needed to recover data from server app
-    "author": "Christoph Hoenes",
-    // needed to recover data from server app
-    "experimentID": "6",
-    "description": "Replica study by Hoenes and Schreiber",
+    // the experimentID is needed to recover data from the _babe server app
+	// you receive the experimentID when you create the experiment using the _babe server app
+
+	 "experimentID": "6",
 
     // set deployment method; use one of:
     //'debug', 'localServer', 'MTurk',
@@ -16,5 +15,5 @@ var config_deploy = {
     // optional fields
 
     // who to contact in case of trouble
-    "contact_email": "christoph.hoenes@gmail.com",
+    "contact_email": "YOUREMAIL@wherelifeisgreat.you",
 };
